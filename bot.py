9f812e2f8b5f8a5b41b9387a1f3b54eb76a0d3ff
Adarsha Masalkar <97529973+adarsha-masalkar@users.noreply.github.com>
2022-12-16 17:37:38 +0530

    Initial commit

---
+++ bot.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import pandas as pd
+
+url = 'https://www.imdb.com/'
+
+movies_list = []
+
+next_url = ['search/title/?release_date=2018-01-01,2018-12-31&sort=num_votes,desc']
+
+
+def page(c):
+    curr_url = url + c
+    response = requests.get(curr_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    movies = soup.select('h3.lister-item-header a')
+    runtime = soup.select('span.runtime')
+    id1 = soup.select('span.lister-item-index')
+
+    for a, b, c in zip(movies, runtime, id1):
+        # movies_list[c.get_text()[:-1]] = (a.get_text(), b.get_text()[:-4])
+        movies_list.append((c.get_text()[:-1], a.get_text(), b.get_text()))
+
+    next1 = soup.select('a.lister-page-next')[0]
+    next_url.append(next1.get('href'))
+
+
+cur = 0
+for i in next_url:
+    if cur <= 170:
+        page(i)
+        print(cur)
+        cur += 1
+    else:
+        movies_list.sort(key=lambda x: float(x[2][:-4]), reverse=True)
+        for i in movies_list:
+            if 1000 <= int(i[0].replace(',', '')) <= 8000:
+                print(i)
+                exit()
